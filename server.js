8a8518969a465af19cc26a0aa8da3486bf9d0215
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-04-29 10:08:27 -0600

    link up app to mongoDB cloud database

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Initial server file
 const express = require('express');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 
+// Initialize instance of express
 const app = express();
 
+// Connect to the Atlas cloud database
+connectDB();
+
+// Test API route
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 6000;
