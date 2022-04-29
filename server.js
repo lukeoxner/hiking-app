ccd93e9ba7a9a2b608af36f48de6447afc0381d3
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-04-29 10:00:16 -0600

    initial express setup

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 // Initial server file
 const express = require('express');
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => console.log(`Server spooled up on port ${PORT}`));
